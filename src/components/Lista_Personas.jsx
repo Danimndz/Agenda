eb7a5bf7766fe0ba614b17411d8c6657e4a65b25
Danimndz <wachun4m3@gmail.com>
2021-03-25 22:10:14 -0600

    finish list-persons

--- src/components/Lista_Personas.jsx
+++ src/components/Lista_Personas.jsx
@@ -1,19 +1,50 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { Button, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../Redux/actions/fetchData";
 
-function Lista_Personas(props) {
-  props.fetchData();
-  return (
-    <div>
-      <h1>LISTADO PERSONAS</h1>
-    </div>
-  );
-}
+const Lista_Personas = (props) => {
+  const persons = useSelector((state) => state.persons);
+  const dispatch = useDispatch();
 
-const mapDispatchActions = {
-  fetchData: fetchData,
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
+  return (
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>last name</th>
+            <th>Age</th>
+            <th>Phone</th>
+            <th>Occupation</th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          {persons.map((person,idx)=>{
+            return(
+              <tr key={idx}>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+              <td>{person.last_name}</td>
+              <td>{person.age}</td>
+              <td>{person.phone}</td>
+              <td>{person.occupation}</td>
+              <td>
+                <Button>Edit</Button>
+              </td>
+              
+            </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+
+  );
 };
 
-export default connect(null, mapDispatchActions)(Lista_Personas);
+export default Lista_Personas;
